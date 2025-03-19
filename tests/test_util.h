b17ed73806a6cc4472f5dce0681b276339dfadcc
Go Kudo <zeriyoshi@gmail.com>
2025-03-19 16:55:57 +0000

    fix(tests): overhaul

--- tests/test_util.h
+++ tests/test_util.h
@@ -62,6 +62,40 @@ static inline bool break_data(uint8_t *data, size_t data_size, uint32_t count)
     return true;
 }
 
+static inline bool break_data_erasure(uint8_t *data, size_t data_size, uint32_t count, poporon_erasure_t *erasure)
+{
+    uint32_t corrupted_count = 0;
+    size_t pos;
+
+    if (!data || data_size == 0 || count == 0) {
+        return false;
+    }
+
+    if (count > data_size) {
+        count = data_size;
+    }
+    
+    bool *corrupted = (bool *)calloc(data_size, sizeof(bool));
+    if (!corrupted) {
+        return false;
+    }
+    
+    while (corrupted_count < count) {
+        pos = rand() % data_size;
+        
+        if (!corrupted[pos]) {
+            data[pos] ^= 0xFF;
+            corrupted[pos] = true;
+            poporon_erasure_add_position(erasure, pos);
+            corrupted_count++;
+        }
+    }
+    
+    free(corrupted);
+
+    return true;
+}
+
 static inline void print_hex(uint8_t *data, size_t len)
 {
     for (size_t i = 0; i < len; i++) {
